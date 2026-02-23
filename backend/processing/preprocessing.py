ac88d94c937fe480d67ab7e4d2602bde7bea15b2
Ali Al-Mokdad <am.compsci@gmail.com>
2026-02-23 15:50:12 +0200

    Add key detection algorithm

--- backend/processing/preprocessing.py
+++ backend/processing/preprocessing.py
@@ -15,6 +15,53 @@ def get_midi_data(audio_filepath):
     _, midi_data, _ = predict(audio_filepath)
     return midi_data
 
+import numpy as np
+
+def detect_key_from_histogram(histogram):
+    """
+    A simple implementation of the Krumhansl-Schmuckler key-finding algorithm.
+    The histogram should contain the durations of notes in the order below (NOTE_NAMES).
+    """
+    MAJOR_PROFILE = np.array([
+        6.35, 2.23, 3.48, 2.33, 4.38, 4.09,
+        2.52, 5.19, 2.39, 3.66, 2.29, 2.8
+    ])
+
+    MINOR_PROFILE = np.array([
+        6.33, 2.68, 3.52, 5.38, 2.60, 3.53,
+        2.54, 4.75, 3.98, 2.69, 3.34, 3.17
+    ])
+
+    NOTE_NAMES = np.array([
+        "C", "C#", "D", "D#", "E", "F",
+        "F#", "G", "G#", "A", "A#", "B"
+    ])
+
+    if histogram.sum() == 0:
+        return None
+
+    histogram = histogram / histogram.sum()
+
+    best_score = -np.inf
+    best_key = None
+
+    for i in range(12):
+        major_rot = np.roll(MAJOR_PROFILE, i)
+        minor_rot = np.roll(MINOR_PROFILE, i)
+
+        major_score = np.corrcoef(histogram, major_rot)[0, 1]
+        minor_score = np.corrcoef(histogram, minor_rot)[0, 1]
+
+        if major_score > best_score:
+            best_score = major_score
+            best_key = f"{NOTE_NAMES[i]}-major"
+
+        if minor_score > best_score:
+            best_score = minor_score
+            best_key = f"{NOTE_NAMES[i]}-minor"
+
+    return best_key
+
 def midi_to_model_format(midi_data):
     """
     Converts to the format used for the Markov model, which is:
@@ -58,8 +105,6 @@ def midi_to_model_format(midi_data):
     # this is the actual note to format step
     events = []
     current_time = round(filtered_notes[0].start / step_duration) * step_duration
-
-    # this is the actual note to format step
     for note in filtered_notes:
         rest_steps = max(0, round((note.start - current_time) / step_duration))
         if rest_steps > 0:
