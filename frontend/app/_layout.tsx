573c676363eb03a5efd6d8faf309645316e0a17a
Ali Al-Mokdad <am.compsci@gmail.com>
2026-05-06 12:27:41 +0300

    Add basic home page

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,61 @@
-import { Stack } from "expo-router";
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { Link, Stack } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Markov Music",
+          headerTitleStyle: {
+            fontFamily: "Inter_700Bold",
+          },
+          headerRight: () => (
+            <Link href="/list" asChild>
+              <Pressable>
+                <Text
+                  style={{
+                    color: "black",
+                    marginRight: 10,
+                    fontFamily: "Inter_400Regular",
+                    fontSize: 18,
+                  }}
+                >
+                  List
+                </Text>
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="list"
+        options={{
+          title: "List",
+          headerTitleStyle: {
+            fontFamily: "Inter_700Bold",
+          },
+        }}
+      />
+    </Stack>
+  );
 }
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.mainBox}>
+        <Text style={styles.mainText}>
+          Record or upload a musical phrase. We'll help you get inspired.
+        </Text>
+      </View>
+
+      <View style={styles.bottomBox}>
+        <Pressable>
+          <Text>Record</Text>
+        </Pressable>
+
+        <Pressable>
+          <Text>Upload File</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+
+  mainBox: {
+    marginTop: 20,
+    padding: 20,
+  },
+
+  mainText: {
+    fontFamily: "Inter_400Regular",
+    fontSize: 35,
+  },
+
+  bottomBox: {
+    marginBottom: 100,
+    padding: 20,
+  },
+});
